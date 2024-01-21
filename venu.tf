d8d550d3e49c19e39889076c2af1ccd41600a8e7
venu <agurlavenu1@gmail.com>
2024-01-21 19:50:33 +0530

    commit

--- venu.tf
+++ venu.tf
@@ -11,6 +11,7 @@ resource "aws_security_group" "av" {
 resource "aws_instance" "web" {
    ami = "ami-0d2bc8073c06a612f"
    instance_type = "t2.large"
+   vpc_Id = aws_vpc.venu.id
    vpc_security_group_ids = [aws_security_group.av.id]
    tags = {
      Name = "HelloWorld"
