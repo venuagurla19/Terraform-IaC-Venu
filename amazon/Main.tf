0f13fb2c414ca5e8ae9c5f5e8f9d20b1d008805f
venu <agurlavenu1@gmail.com>
2024-01-21 10:00:23 +0530

    first commit

--- amazon/Main.tf
+++ amazon/Main.tf
@@ -4,13 +4,12 @@ cidr_block = "CIDR Block"
 resource "resource "aws_security_group" "allow_tls" {
 name        = "allow_tls"
 description = "Open 22,443,80,8080,9000,9100,9090,3000"
-
 vpc_id      = aws_vpc.my_vpc.id
 
 ingress {
     description = "TLS from VPC"
-    from_port   = port
-    to_port     = port
+    from_port   = 22
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 }
