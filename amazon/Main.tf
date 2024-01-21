1d7952b5f57544121e50e18ef0861668e4134a81
venu <agurlavenu1@gmail.com>
2024-01-21 10:02:28 +0530

    first commit

--- amazon/Main.tf
+++ amazon/Main.tf
@@ -1,29 +1,28 @@
-resource = resource "aws_vpc" "my_vpc" {
-cidr_block = "CIDR Block"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "CIDR Block"
 }
-resource "resource "aws_security_group" "allow_tls" {
-name        = "allow_tls"
-description = "Open 22,443,80,8080,9000,9100,9090,3000"
-vpc_id      = aws_vpc.my_vpc.id
 
-ingress {
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Open 22,443,80,8080,9000,9100,9090,3000"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
     description = "TLS from VPC"
     from_port   = 22
-    to_port     = 3000
+    to_port     = 3000  # Assuming you want to open ports 22 to 3000, adjust as needed
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-}
+  }
 
-egress {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-}
+  }
 
-tags = {
+  tags = {
     Name = "allow_tls"
-}
-}" "name" {
-
+  }
 }
