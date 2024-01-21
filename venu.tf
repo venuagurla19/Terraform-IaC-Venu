aec56448dc531cf79ab10e08667f71e9755c7527
venu <agurlavenu1@gmail.com>
2024-01-21 20:50:05 +0530

    commit

--- venu.tf
+++ venu.tf
@@ -1,18 +1,3 @@
-resource "aws_vpc" "venu" {
-    cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_security_group" "av" {
-    name = "av"
-    description = "Allow all inbound traffic."
-    vpc_id      = aws_vpc.venu.id
-  
-}
-resource "aws_instance" "web" {
-   ami = "ami-0d2bc8073c06a612f"
-   instance_type = "t2.large"
-   vpc_security_group_ids = [aws_security_group.av.id]
-   tags = {
-     Name = "HelloWorld"
-   }
+resource "aws_vpc" "venu-id" {
+  cidr_block = "10.0.0.0/16"
 }
