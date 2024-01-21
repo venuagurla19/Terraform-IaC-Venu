a6cc50bf87d4cc7de566ea7eba1ddc6314ae37ad
venu <agurlavenu1@gmail.com>
2024-01-21 10:20:55 +0530

    first commit

--- amazon/Main.tf
+++ amazon/Main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = "CIDR Block"
 }
 
-resource "aws_security_group" "allow_tls" {
+resource "aws_security_group" "Jenkins-sg" {
   name        = "allow_tls"
   description = "Open 22,443,80,8080,9000,9100,9090,3000"
   vpc_id      = aws_vpc.my_vpc.id
@@ -23,6 +23,33 @@ resource "aws_security_group" "allow_tls" {
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "jenkins-sg"
   }
+}
+
+resource 'resource "aws_instance" "web" {
+   ami             = "ami-0d2bc8073c06a612f"
+   instance_type   = "t2.large"
+   key_name        = "ohio"
+   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+   user_data              = templatefile("./install_jenkins.sh",{})
+   
+   tags = {
+    Name = "amazon clone"
+   }
+   root_block-device {
+    volume_size = 30
+   }
+}
+
+resource 'resource "aws_instance" "web2" {
+   ami = "ami-0d2bc8073c06a612f"
+   instance_type = "t2.medium"
+   key_name      = "ohio"
+    vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+   tags {
+     Name = "monitoring via Grafana"
+   }
+   root_block-device {
+    volume_size = 30
 }
