6c56a6894f7d76c7962cb4e8566f27dce96315b3
venu <agurlavenu1@gmail.com>
2024-01-24 20:10:36 +0530

    commit

--- amazon/Main.tf
+++ amazon/Main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public" {
   cidr_block = "10.0.1.0/24"
-  map_customer_owned_ip_on_launch = true
+  map_public_ip_on_launch = true
   vpc_id = aws_vpc.advik-id.id
   availability_zone = "us-east-2a"
 }
